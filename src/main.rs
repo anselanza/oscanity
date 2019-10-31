bb0d5e25dae863cfce9ead21c50bd4cbd007fb57
Stephen Buchanan <stephen.buchanan@random.studio>
2019-10-31 12:13:20 +0100

    init commit, receiving example

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+extern crate rosc;
+
+use rosc::OscPacket;
+use std::env;
+use std::net::{SocketAddrV4, UdpSocket};
+use std::str::FromStr;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let usage = format!("Usage {} IP:PORT", &args[0]);
+    if args.len() < 2 {
+        println!("{}", usage);
+        ::std::process::exit(1)
+    }
+    let addr = match SocketAddrV4::from_str(&args[1]) {
+        Ok(addr) => addr,
+        Err(_) => panic!(usage),
+    };
+    let sock = UdpSocket::bind(addr).unwrap();
+    println!("Listening to {}", addr);
+
+    let mut buf = [0u8; rosc::decoder::MTU];
+
+    loop {
+        match sock.recv_from(&mut buf) {
+            Ok((size, addr)) => {
+                println!("Received packet (length {}) from: {}", size, addr);
+                let packet = rosc::decoder::decode(&buf[..size]).unwrap();
+                handle_packet(packet);
+            }
+            Err(e) => {
+                println!("Error receiving from socket: {}", e);
+                break;
+            }
+        }
+    }
+}
+
+fn handle_packet(packet: OscPacket) {
+    match packet {
+        OscPacket::Message(msg) => {
+            println!("OSC address: {}", msg.addr);
+            let arg_list = match msg.args {
+                Some(args) => {
+                    format!("{:?}", args)
+                }
+                None => {
+                   String::from("zero")
+                }
+            };
+            println!("Args: {}", arg_list);
+        }
+        OscPacket::Bundle(bundle) => {
+            println!("OSC Bundle: {:?}", bundle);
+        }
+    }
+}
