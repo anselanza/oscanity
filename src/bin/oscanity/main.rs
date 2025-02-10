253e58a08df439b2642ffca234dbc5a9d7f0ae93
Stephen Buchanan <webtvmaster@gmail.com>
2025-02-10 13:59:01 +0100

    Basic clap subcommands working with receive only

---
+++ src/bin/oscanity/main.rs
@@ -0,0 +1,33 @@
+use clap::{Parser, Subcommand};
+use env_logger::{Builder, Env};
+use log::*;
+use oscanity::receive::{receive_osc, ReceiveOptions};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
+    #[arg(long = "loglevel",default_value_t=String::from("info"))]
+    pub log_level: String,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Receive(ReceiveOptions),
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    env_logger::Builder::from_env(Env::default().default_filter_or(&cli.log_level))
+        // .filter_module("rumqttc", log::LevelFilter::Warn)
+        .init();
+
+    debug!("Debug log level is enabled; could be verbose");
+
+    match &cli.command {
+        Commands::Receive(receive_options) => receive_osc(receive_options),
+    }
+}
--- src/receive.rs
+++ src/receive.rs
@@ -1,21 +1,34 @@
 extern crate rosc;
 
+use clap::Args;
+use log::*;
 use rosc::OscPacket;
-use std::env;
 use std::net::{SocketAddrV4, UdpSocket};
 use std::str::FromStr;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let usage = format!("Usage: {} RECEIVE_IP:RECEIVE_PORT", &args[0]);
-    if args.len() < 2 {
-        println!("{}", &usage);
-        panic!()
-    }
-    let receive_address = get_addr_from_arg(&args[1]);
+#[derive(Args, Default)]
+pub struct ReceiveOptions {
+    #[arg(long = "host", default_value_t = String::from("127.0.0.1"))]
+    pub host: String,
 
-    let socket = UdpSocket::bind(receive_address).expect("Error binding udp socket");
-    println!("Listening to {}", receive_address);
+    #[arg(long = "port", default_value_t = 12345)]
+    pub port: usize,
+}
+
+pub fn receive_osc(options: &ReceiveOptions) {
+    // let args: Vec<String> = env::args().collect();
+    // let usage = format!("Usage: {} RECEIVE_IP:RECEIVE_PORT", &args[0]);
+    // if args.len() < 2 {
+    //     println!("{}", &usage);
+    //     panic!()
+    // }
+    // let receive_address = get_addr_from_arg(&args[1]);
+    let receive_url = format!("{}:{}", options.host, options.port);
+    info!("Will listen at {}", &receive_url);
+
+    let socket = UdpSocket::bind(&receive_url).expect("Error binding udp socket");
+    // println!("Listening to {}", receive_address);
+    //
 
     let mut receive_buffer = [0u8; rosc::decoder::MTU];
 
